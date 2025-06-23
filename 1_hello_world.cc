5e8260eb01141488c96bf5b769329929660cb972
supericeliu <supericeliu@tencent.com>
2025-06-23 20:34:00 +0800

    更新demo

--- 1_hello_world.cc
+++ 1_hello_world.cc
@@ -1,7 +1,9 @@
 #include <node.h>
 #include <v8.h>
+#include <uv.h>
 
 void Method(const v8::FunctionCallbackInfo<v8::Value>& args) {
+  uv_sleep(1000);
   args.GetReturnValue().Set(v8::String::NewFromUtf8(
     args.GetIsolate(), "Hello World").ToLocalChecked());
 }
--- 1_hello_world.js
+++ 1_hello_world.js
@@ -1,3 +1,5 @@
 const addon = require('./1_hello_world.node')
 
 console.log(addon.hello())
+
+console.log('终于执行到了下一行');
